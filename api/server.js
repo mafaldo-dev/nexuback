7dad4944c66c2d299aae0e22b9a432c7532a610f
Guilherme <03.09gui.mafaldo@gmail.com>
2025-03-29 10:38:30 -0300

    fix: .

--- api/server.js
+++ api/server.js
@@ -1,6 +1,6 @@
 import express from "express";
 import cors from "cors";
-import redis from "redis";
+import { createClient } from "redis"; // Importar a nova versão do Redis
 import dotenv from "dotenv";
 import searchAll from "./engine.js";
 
@@ -11,11 +11,11 @@ const app = express();
 const PORT = 3000;
 
 // Configuração do Redis
-const client = redis.createClient({
-  host: process.env.REDIS_HOST,
-  port: process.env.REDIS_PORT,
-  password: process.env.REDIS_PASSWORD,
+const client = createClient({
+  url: `redis://:${process.env.REDIS_PASSWORD}@${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`
 });
+
+client.connect(); // Conecta ao Redis
 
 client.on("connect", () => {
   console.log("Conectado ao Redis");
@@ -25,23 +25,20 @@ client.on("error", (err) => {
   console.error("Erro ao conectar ao Redis:", err);
 });
 
+// Configuração do CORS
 const corsOptions = {
-  origin: "*",
-  methods: ["GET", "OPTIONS"],
+  origin: "*", // Aceitar todas as origens
+  methods: ["GET", "OPTIONS"], // Aceitar apenas GET e OPTIONS
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true
 };
-app.use(cors(corsOptions));
 
+app.use(cors(corsOptions)); // Middleware para CORS
 app.use(express.json()); // Habilita JSON no Express
 
 // Rota de busca com cache no Redis
-app.get("/api/searchAll", cors(corsOptions), async (req, res) => {
+app.get("/api/searchAll", async (req, res) => {
   const query = req.query.q;
-
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 
   if (!query) {
     return res.status(400).json({ error: "É necessário fornecer um termo de busca" });
@@ -63,7 +60,7 @@ app.get("/api/searchAll", cors(corsOptions), async (req, res) => {
       const searchResults = await searchAll(query);
 
       // Armazenar os resultados no cache Redis com um tempo de expiração de 1 hora (3600 segundos)
-      client.setex(query, 3600, JSON.stringify(searchResults));
+      client.setEx(query, 3600, JSON.stringify(searchResults));
 
       return res.status(200).json(searchResults); // Retorna os resultados da pesquisa
     } catch (error) {
